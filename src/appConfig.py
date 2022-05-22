0401b0162b88b39c94f8a27b6f1f91e479f69d4c
= <=>
2022-05-22 14:36:45 -0500

    Implementacion swagger

--- src/appConfig.py
+++ src/appConfig.py
@@ -2,16 +2,30 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from routes.index import index_bp 
+from flask_swagger_ui import get_swaggerui_blueprint
 
 # Inicializar flask
 app = Flask(__name__)
+BASE_PATH=app.root_path
+
+#Swagger config
+SWAGGER_URL = '/Docs'
+API_URL = '/static/swagger.json'
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint (
+    SWAGGER_URL,API_URL,config={
+        'app_name':'Curriculums_ex'
+    }
+)
+
 
 # Registrar Blueprints de rutas
+app.register_blueprint(SWAGGER_BLUEPRINT,url_prefix = SWAGGER_URL)
 app.register_blueprint(index_bp)
 
 # Conexion a mongo
 app.config['MONGO_URI']='mongodb+srv://Curr_ADMIN:Kv4eGREjttoWv7qL@curriculumsdb.rd9hs.mongodb.net/Curriculums_ex?retryWrites=true&w=majority'
 
+#Inicializacion conexion a mongo
 mongo = PyMongo(app)
 
 
--- src/routes/index.py
+++ src/routes/index.py
@@ -1,11 +1,28 @@
-from flask import Blueprint,request
+from urllib import response
+from flask import Blueprint, jsonify,request
 import appConfig
 
 # Creacion blueprint
 index_bp = Blueprint('index',__name__)
 
 # Ruta inicial
-@index_bp.route('/',methods=['POST'])
+@index_bp.route('/',methods=['GET'])
 def index():
-    
-    return {'mensaje': 'Recibido'}
+    response = jsonify({
+        'message':'Bienvenido',
+        'status':200
+    })
+   
+    return response
+
+@index_bp.errorhandler(404)
+def error_404(error=None):
+    response = jsonify({
+        'message':f'Recurso no encontrado{request.url}',
+        'status':200
+    })
+    response.status_code = 404
+
+    return response
+
+
